16dc75c4c05833cfd28499637e6dbfa67269e826
Gabababab <gravottagabriele@gmail.com>
2021-10-30 18:24:42 +0200

    primo commit, implementazione Model

---
+++ src/main/java/it/prova/myebay/model/Acquisto.java
@@ -0,0 +1,33 @@
+package it.prova.myebay.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "acquisto")
+public class Acquisto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "prezzo")
+	private Integer prezzo;
+	@Column(name = "dataacquisto")
+	private Date dataAcquisto;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utenteAcquirente;
+}
---
+++ src/main/java/it/prova/myebay/model/Annuncio.java
@@ -0,0 +1,109 @@
+package it.prova.myebay.model;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "annuncio")
+public class Annuncio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "testoannuncio")
+	private String testoAnnuncio;
+	@Column(name = "prezzo")
+	private Integer prezzo;
+	@Column(name = "datapubblicazione")
+	private Date dataPubblicazione;
+	@Column(name = "aperto")
+	private Boolean aperto;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utenteInserimento;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "annuncio_categoria", joinColumns = @JoinColumn(name = "annuncio_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "categoria_id", referencedColumnName = "ID"))
+	private Set<Categoria> categorie= new HashSet<Categoria>();
+
+	public Annuncio(String testoAnnuncio, Integer prezzo, Date dataPubblicazione) {
+		super();
+		this.testoAnnuncio = testoAnnuncio;
+		this.prezzo = prezzo;
+		this.dataPubblicazione = dataPubblicazione;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTestoAnnuncio() {
+		return testoAnnuncio;
+	}
+
+	public void setTestoAnnuncio(String testoAnnuncio) {
+		this.testoAnnuncio = testoAnnuncio;
+	}
+
+	public Integer getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Integer prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public Date getDataPubblicazione() {
+		return dataPubblicazione;
+	}
+
+	public void setDataPubblicazione(Date dataPubblicazione) {
+		this.dataPubblicazione = dataPubblicazione;
+	}
+
+	public Utente getUtenteInserimento() {
+		return utenteInserimento;
+	}
+
+	public void setUtenteInserimento(Utente utenteInserimento) {
+		this.utenteInserimento = utenteInserimento;
+	}
+
+	public Set<Categoria> getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(Set<Categoria> categorie) {
+		this.categorie = categorie;
+	}
+
+	public Boolean getAperto() {
+		return aperto;
+	}
+
+	public void setAperto(Boolean aperto) {
+		this.aperto = aperto;
+	}
+	
+	
+}
